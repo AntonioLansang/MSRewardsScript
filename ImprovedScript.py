7a4a7f9fc32d886bd27956818f1d1edfe5d8dac6
TheCrimsonTitan <antoniolansang500@gmail.com>
2023-11-06 12:12:48 -0600

    Found that html isn't showing. JS is involved. We will need to change our approach

--- ImprovedScript.py
+++ ImprovedScript.py
@@ -13,12 +13,10 @@ def main():
     #Use Bs4 to grab all the content from the webpage
     Soup=BeautifulSoup(html.content, 'html.parser')
 
-    
+    #Soup isn't showing the "pntile" tag of lists. Hmmm...
+    print(Soup) 
     LinksForNews=Soup.find_all('li', class_='pntile')
     print(LinksForNews)
-
-#    for link in Soup.find_all('li', class_='pntile'):
-#        print(link.get_text())
 
 if __name__ == '__main__':
     main()
